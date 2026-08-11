68fd51e2aa81ab8d03250a3b33d19ea116e67145
shallow0613 <2014610565@qq.com>
2026-08-12 03:09:09 +0800

    style: format csv_load function and improve readability

--- common/csv_util.py
+++ common/csv_util.py
@@ -1,5 +1,9 @@
 import csv
+import os
 
-def csv_load(url:str):
+
+def csv_load(url: str):
+    if os.sep != '\\':
+        url = url.replace('\\', os.sep)
     with open(url,'r',encoding='utf-8') as f:
         return list(csv.reader(f))[1:]
